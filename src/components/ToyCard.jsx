108069fb35980ed754a4f353cecaaedead08a3aa
Mari <demarius.edwards@gmail.com>
2020-03-11 20:57:52 -0400

    got up to delete

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 
 class ToyCard extends Component {
-
+  handleDelete = (e) => {
+    this.props.deleteOneToy(this.props.toyObj.id)
+  }
   render() {
+    console.log(this.props)
+    
     return (
       <div className="card">
-        <h2>{'' /* Toy's Name */}</h2>
-        <img src={'' /* Toy's Image */} alt={/* Toy's Name */} className="toy-avatar" />
-        <p>{'' /* Toy's Likes */} Likes </p>
+        <h2>{this.props.name}</h2>
+        <img src={this.props.image} alt={this.props.name} className="toy-avatar" />
+        <p>{this.props.likes} Likes </p>
         <button className="like-btn">Like {'<3'}</button>
-        <button className="del-btn">Donate to GoodWill</button>
+        <button className="del-btn" onClick={this.handleDelete}>Donate to GoodWill</button>
       </div>
     );
   }
