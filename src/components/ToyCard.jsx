6dced997e52e387e9ccf210e411d038e432f31a7
github username <github email address>
2020-03-11 22:28:57 -0400

    renders toy cards to DOM

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -3,11 +3,14 @@ import React, { Component } from 'react';
 class ToyCard extends Component {
 
   render() {
+    let {name, image, likes} = this.props.toyObj
+
+
     return (
       <div className="card">
-        <h2>{'' /* Toy's Name */}</h2>
-        <img src={'' /* Toy's Image */} alt={/* Toy's Name */} className="toy-avatar" />
-        <p>{'' /* Toy's Likes */} Likes </p>
+        <h2>{name /* Toy's Name */}</h2>
+        <img src={image} alt={name} className="toy-avatar" />
+        <p>{likes} Likes </p>
         <button className="like-btn">Like {'<3'}</button>
         <button className="del-btn">Donate to GoodWill</button>
       </div>
--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import ToyCard from './ToyCard'
+// import toyData from '../data';
 
-const ToyContainer = () => {
+const ToyContainer = (props) => {
+  // create POJOs to pass to ToyCard
+
+  let arrayOfToyComponents = props.toys.map((toyPOJO, index, theOriginalArray) => {
+    return <ToyCard key={toyPOJO.id}
+      toyObj={toyPOJO}
+    />
+  })
+
+
   return(
     <div id="toy-collection">
-      {/* Render the collection of ToyCards */}
+      {/* Render the collection of ToyCards 
+      why doesn't this work? --> this.props.toys */
+        arrayOfToyComponents
+    
+      }
     </div>
   );
 }
